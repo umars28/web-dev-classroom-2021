06d1c467e7366bf377be5cc29f44736e43afc68d
Umar Sabirin <umarsabirin@fairtech.com.sg>
2021-09-29 00:37:29 +0800

    add confirm message when delete item

--- 04-js/todo.js
+++ 04-js/todo.js
@@ -10,18 +10,19 @@ function addElement() {
                 <div class="card-body">
                   <h5 class="card-title">${inputValue}</h5>
                 </div>
-                <button class="delete btn btn-outline-danger">Close</button>
+                <button class="delete btn btn-outline-danger" onclick="deleteElement()">Delete</button>
               </div>
             </div>`;
-
-    deleteElement();
 }
 
 function deleteElement() {
-    const deleteElementByClass = document.getElementsByClassName("delete");
+  const deleteElementByClass = document.getElementsByClassName("delete");
     for (var i = 0; i < deleteElementByClass.length; i++) {
-        deleteElementByClass[i].onclick = function() {
-            this.parentElement.style.display = "none";
-        }
+      deleteElementByClass[i].onclick = function() {
+        var isConfirmDelete = confirm("Konfirmasi Penghapusan!");
+        if (isConfirmDelete == true) {
+          this.parentElement.style.display = "none";
+        }  
+      }
     }
 }
